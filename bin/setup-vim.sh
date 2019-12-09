7c113c8e78be1a452d025a4d67bcd6f8996221a0
Marco Morais <marcoemorais@yahoo.com>
2019-12-09 11:25:24 -0800

    Update vim plugins

--- bin/setup-vim.sh
+++ bin/setup-vim.sh
@@ -3,19 +3,19 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-NERDTREE_TAG=5.0.0
+NERDTREE_TAG=6.3.0
 mkdir -p ~/.vim/bundle && \
     git clone --recursive -q https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree --branch $NERDTREE_TAG
 
-CTRLP_TAG=2.80
+CTRLP_TAG=master
 mkdir -p ~/.vim/bundle && \
     git clone --recursive -q https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim --branch $CTRLP_TAG
 
-VIM_MAN_TAG=v0.1.0
+VIM_MAN_TAG=master
 mkdir -p ~/.vim/bundle && \
     git clone --recursive -q https://github.com/vim-utils/vim-man.git ~/.vim/bundle/vim-man --branch $VIM_MAN_TAG
 
-TAGBAR_TAG=v2.7
+TAGBAR_TAG=master
 mkdir -p ~/.vim/bundle && \
     git clone --recursive -q https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar --branch $TAGBAR_TAG
 
@@ -27,11 +27,11 @@ VIM_PROTOBUF_TAG=master
 mkdir -p ~/.vim/bundle && \
     git clone --recursive -q https://github.com/uarun/vim-protobuf.git ~/.vim/bundle/vim-protobuf --branch $VIM_PROTOBUF_TAG
 
-VIM_MARKDOWN_TAG=2.0.0
+VIM_MARKDOWN_TAG=master
 mkdir -p ~/.vim/bundle && \
     git clone --recursive -q https://github.com/plasticboy/vim-markdown.git ~/.vim/bundle/vim-markdown --branch $VIM_MARKDOWN_TAG
 
-VIM_GO_TAG=v1.19
+VIM_GO_TAG=v1.21
 mkdir -p ~/.vim/bundle && \
     git clone --recursive -q https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go --branch $VIM_GO_TAG
 
