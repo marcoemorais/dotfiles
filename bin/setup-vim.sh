c9520207a18739a5e515863c91378a8b603ea3ad
Marco Morais <marcoemorais@yahoo.com>
2022-01-17 13:59:25 -0800

    Update vim setup

--- bin/setup-vim.sh
+++ bin/setup-vim.sh
@@ -1,37 +1,24 @@
 #!/usr/bin/env bash
 
+# Install pathogen.
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-NERDTREE_TAG=6.7.15
-mkdir -p ~/.vim/bundle && \
-    git clone --recursive -q https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree --branch $NERDTREE_TAG
+# Install vim plugins.
+GUTENTAGS_TAG=master
+git clone https://github.com/ludovicchabant/vim-gutentags.git ~/.vim/bundle/vim-gutentags --branch $GUTENTAGS_TAG
 
-CTRLP_TAG=master
-mkdir -p ~/.vim/bundle && \
-    git clone --recursive -q https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim --branch $CTRLP_TAG
+NERDTREE_TAG=6.10.16
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree --branch $NERDTREE_TAG
 
 VIM_MAN_TAG=master
-mkdir -p ~/.vim/bundle && \
-    git clone --recursive -q https://github.com/vim-utils/vim-man.git ~/.vim/bundle/vim-man --branch $VIM_MAN_TAG
+git clone https://github.com/vim-utils/vim-man.git ~/.vim/bundle/vim-man --branch $VIM_MAN_TAG
 
-TAGBAR_TAG=master
-mkdir -p ~/.vim/bundle && \
-    git clone --recursive -q https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar --branch $TAGBAR_TAG
+CTRLP_TAG=1.81
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp --branch $CTRLP_TAG
 
-PIG_VIM_TAG=master
-mkdir -p ~/.vim/bundle && \
-    git clone --recursive -q https://github.com/motus/pig.vim.git ~/.vim/bundle/pig.vim --branch $PIG_VIM_TAG
+VIM_GO_TAG=v1.25
+git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go --branch $VIM_GO_TAG
 
-VIM_PROTOBUF_TAG=master
-mkdir -p ~/.vim/bundle && \
-    git clone --recursive -q https://github.com/uarun/vim-protobuf.git ~/.vim/bundle/vim-protobuf --branch $VIM_PROTOBUF_TAG
-
-VIM_MARKDOWN_TAG=master
-mkdir -p ~/.vim/bundle && \
-    git clone --recursive -q https://github.com/plasticboy/vim-markdown.git ~/.vim/bundle/vim-markdown --branch $VIM_MARKDOWN_TAG
-
-VIM_GO_TAG=v1.23
-mkdir -p ~/.vim/bundle && \
-    git clone --recursive -q https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go --branch $VIM_GO_TAG
-
+VIM_RIPGREP_TAG=v1.0.3
+git clone https://github.com/jremmen/vim-ripgrep.git ~/.vim/bundle/vim-ripgrep --branch $VIM_RIPGREP_TAG
