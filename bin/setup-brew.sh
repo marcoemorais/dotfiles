99c4db5d9a85703b9356915ccb9a16268c21fef7
Marco Morais <marcoemorais@yahoo.com>
2022-01-17 14:00:38 -0800

    Update brew package list

--- bin/setup-brew.sh
+++ bin/setup-brew.sh
@@ -3,12 +3,15 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install \
-    ctags \
+    ccache \
+    direnv \
     go \
     htop \
     jq \
     pyenv \
+    ripgrep \
     tmux \
     tree \
+    universal-ctags \
     zlib
 
