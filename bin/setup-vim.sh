f8fe80e02a6532e44f76b4a1cc1a90dc763a0acb
Marco Morais <marcoemorais@yahoo.com>
2024-06-02 22:19:45 -0700

    Update vim plugins

--- bin/setup-vim.sh
+++ bin/setup-vim.sh
@@ -8,7 +8,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 GUTENTAGS_TAG=master
 git clone https://github.com/ludovicchabant/vim-gutentags.git ~/.vim/bundle/vim-gutentags --branch $GUTENTAGS_TAG
 
-NERDTREE_TAG=6.10.16
+NERDTREE_TAG=7.1.1
 git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree --branch $NERDTREE_TAG
 
 VIM_MAN_TAG=master
@@ -17,14 +17,14 @@ git clone https://github.com/vim-utils/vim-man.git ~/.vim/bundle/vim-man --branc
 CTRLP_TAG=1.81
 git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp --branch $CTRLP_TAG
 
-VIM_GO_TAG=v1.25
+VIM_GO_TAG=v1.28
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go --branch $VIM_GO_TAG
 
 VIM_RIPGREP_TAG=v1.0.3
 git clone https://github.com/jremmen/vim-ripgrep.git ~/.vim/bundle/vim-ripgrep --branch $VIM_RIPGREP_TAG
 
-TAGBAR_TAG=v3.0.0
+TAGBAR_TAG=v3.1.1
 git clone https://github.com/preservim/tagbar.git ~/.vim/bundle/tagbar --branch $TAGBAR_TAG
 
-EDITORCONFIG_TAG=v1.1.1
+EDITORCONFIG_TAG=v1.2.0
 git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/editorconfig --branch $EDITORCONFIG_TAG
